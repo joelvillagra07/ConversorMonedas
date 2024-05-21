a248f1f32a45493ebe038312ae363ed677592654
Joel Villagra <joel.villagra.07@gmail.com>
2024-05-21 20:54:40 -0300

    actualizacion, he creado las clases httpclient, httprequest, httpresponse

---
+++ src/Monedas.java
@@ -0,0 +1,29 @@
+public class Monedas {
+     private int pesoArgentino;
+   private int realBrasileno;
+   private int dolarEstadounidense;
+
+    public int getPesoArgentino() {
+        return pesoArgentino;
+    }
+
+    public void setPesoArgentino(int pesoArgentino) {
+        this.pesoArgentino = pesoArgentino;
+    }
+
+    public int getRealBrasileno() {
+        return realBrasileno;
+    }
+
+    public void setRealBrasileno(int realBrasileno) {
+        this.realBrasileno = realBrasileno;
+    }
+
+    public int getDolarEstadounidense() {
+        return dolarEstadounidense;
+    }
+
+    public void setDolarEstadounidense(int dolarEstadounidense) {
+        this.dolarEstadounidense = dolarEstadounidense;
+    }
+}
--- src/Principal.java
+++ src/Principal.java
@@ -1,2 +1,32 @@
-public class Principal {
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+
+public  class Principal {
+     public static void main(String[] args) throws IOException, InterruptedException {
+          Scanner teclado = new Scanner(System.in);
+          System.out.println("Ingrese en mayuscula las iniciales de la moneda");
+          var tiposDeCambio = teclado.nextLine();
+          String moneda ="https://v6.exchangerate-api.com/v6/" + "b9d3f34a09fa50078a373291" + "/latest/" + tiposDeCambio ;
+
+          HttpClient client = HttpClient.newHttpClient();
+          HttpRequest request = HttpRequest.newBuilder()
+                  .uri(URI.create(moneda))
+                  .build();
+          HttpResponse<String> response = client
+                  .send(request, HttpResponse.BodyHandlers.ofString());
+          String json = response.body();
+          System.out.println(json);
+          Gson gson = new Gson();
+          Monedas monedas1 = gson.fromJson(json, Monedas.class);
+          System.out.println(monedas1.getDolarEstadounidense());
+
+
+     }
 }
